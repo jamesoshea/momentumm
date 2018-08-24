4b0708d16f846377e44972b69beba457d0ad1b24
James <jo@3yourmind.com>
2018-08-24 16:03:26 +0200

    add confirsmation message to signup flow

--- server/controllers/users/pollTelegram.js
+++ server/controllers/users/pollTelegram.js
@@ -4,7 +4,25 @@ const headers = {
 	'Content-type': 'application/json',
 };
 
-const pollTelegram = async (signupCode) =>
+const sendConfirmationMessage = (chatId, firstName) => {
+	axios
+		.post(
+			`https://api.telegram.org/bot${
+				process.env.MOMENTUMM_TELEGRAM_TOKEN
+			}/sendMessage`,
+			{
+				chat_id: chatId,
+				text: `Thanks, ${firstName}. You can now close Telegram.`,
+			},
+			headers,
+		)
+		.then((response) => response)
+		.catch((error) => {
+			throw error;
+		});
+};
+
+const pollTelegram = (signupCode) =>
 	new Promise((resolve, reject) => {
 		const getTelegramUpdates = () =>
 			axios
@@ -22,6 +40,10 @@ const pollTelegram = async (signupCode) =>
 					if (verificationUpdate) {
 						/* eslint-disable-next-line */
 						clearInterval(polling);
+						sendConfirmationMessage(
+							verificationUpdate.message.chat.id,
+							verificationUpdate.message.from.first_name,
+						);
 						resolve(verificationUpdate.message);
 					}
 				})
